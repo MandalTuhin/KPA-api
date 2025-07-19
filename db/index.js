24c82bc06f37ad926e6fb1a74f2c315bc92807eb
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-07-19 22:41:10 +0530

    feat(db): Configure PostgreSQL connection pool
    This commit introduces the database configuration for the application.
    
    It sets up and exports a reusable connection pool for PostgreSQL using
    the `pg` library. Connection parameters are loaded from environment
    variables via `dotenv` for better security and flexibility across
    different environments.

---
+++ db/index.js
@@ -0,0 +1,19 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+})
+
+// pool.connect().then(() => {
+//     console.log('Connected to the Postgres database');
+// }).catch(err => {
+//     console.error('Postgres Database connection error:', err);
+// });
+
+export default pool; 
